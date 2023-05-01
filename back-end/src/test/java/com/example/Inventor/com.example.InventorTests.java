a307a154c534d786d3bb0229237a417f5b51799a
Ballarja <JamesBallard87@hotmail.com>
2023-04-30 21:14:26 -0500

    Initial commit

---
+++ back-end/src/test/java/com/example/Inventor/com.example.InventorTests.java
@@ -0,0 +1,13 @@
+package com.example.Inventor;
+
+import org.junit.jupiter.api.Test;
+
+
+
+class MainTests{
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ front-end/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Registration from './components/screens/Registration';
+
+
+function App() {
+  return (
+    <Registration/>
+  );
+}
+
+export default App;
